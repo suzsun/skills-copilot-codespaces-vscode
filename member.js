58c9f94ee69025298ccdd9d6a8f1f204a712121a
suzsun <ysun@splunk.com>
2025-02-21 23:58:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    return {
+        name: 'John Doe',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS'],
+        getSkills: function() {
+            return this.skills;
+        }
+    };
+}
